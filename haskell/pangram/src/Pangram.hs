966bf75ae37db3156ce63a2afe03feade8901a37
Sam Frances <sam@samfrances.uk>
2021-09-10 09:07:45 +0100

    Haskell pangram solution, first iteration

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,4 +1,10 @@
 module Pangram (isPangram) where
 
+import Data.List (sort, group)
+import Data.Char (toLower)
+
 isPangram :: String -> Bool
-isPangram text = error "You need to implement this function."
+isPangram text =
+    (getLetters text) == alphabet
+    where getLetters = map head . group . sort . filter (`elem` alphabet) . map toLower
+          alphabet = "abcdefghijklmnopqrstuvwxyz"

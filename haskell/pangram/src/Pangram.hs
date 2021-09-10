26eb153eab82dec2bc7c88d6afc1f999abfaa128
Sam Frances <sam@samfrances.uk>
2021-09-10 13:58:20 +0100

    Refactor Haskell Pangram for readability

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -5,6 +5,9 @@ import Data.Char (toLower)
 
 isPangram :: String -> Bool
 isPangram text =
-    (getLetters text) == alphabet
-    where getLetters = map head . group . sort . filter (`elem` alphabet) . map toLower
+    (countUniqueLetters text) >= 26
+    where countUniqueLetters = length . unique . alphaLetters
+          unique = map head . group . sort
+          alphaLetters = filter isAlpha . map toLower
+          isAlpha = (`elem` alphabet)
           alphabet = "abcdefghijklmnopqrstuvwxyz"

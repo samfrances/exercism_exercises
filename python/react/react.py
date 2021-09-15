998b8d4ff265b434c5316efca9f0bf5b8498a945
Sam Frances <sam@samfrances.uk>
2021-09-15 17:29:49 +0100

    A better attempt to reduce inefficient recomputation

--- python/react/react.py
+++ python/react/react.py
@@ -6,20 +6,9 @@ class Cell:
     def _callbacks(self):
         return set([])
 
-    @property
-    def updating(self):
-        return self._updating
-
     def _notify_callbacks(self):
-        release_callbacks = []
-
         for callback in self._callbacks:
-            release = callback(self.value)
-            if callable(release):
-                release_callbacks.append(release)
-
-        for release in release_callbacks:
-            release()
+            callback(self.value)
 
     def add_callback(self, callback):
         self._callbacks.add(callback)
@@ -31,16 +20,25 @@ class Cell:
 class InputCell(Cell):
     def __init__(self, initial_value):
         self._value = initial_value
+        self._dirty = False
 
     @property
     def value(self):
         return self._value
 
+    @property
+    def dirty(self):
+        return self._dirty
+
     @value.setter
     def value(self, new_val):
         if new_val != self._value:
             self._value = new_val
-            self._notify_callbacks()
+            self._dirty = True
+            try:
+                self._notify_callbacks()
+            finally:
+                self._dirty = False
 
 
 class ComputeCell(Cell):
@@ -48,16 +46,8 @@ class ComputeCell(Cell):
         self._inputs = inputs
         self._compute_function = compute_function
         for input_ in self._inputs:
-            input_.add_callback(
-                ReleasableCallback(
-                    self._recalculate,
-                    self._notify_on_change
-                )
-            )
+            input_.add_callback(self._notify_on_change)
         self._previous_value = self.value
-
-    def _recalculate(self):
-        del self.value
 
     def _notify_on_change(self, value=None):
         new_value = self.value
@@ -65,19 +55,19 @@ class ComputeCell(Cell):
             self._previous_value = new_value
             self._notify_callbacks()
 
+    @property
+    def dirty(self):
+        dirty = any(inp.dirty for inp in self._inputs)
+        return dirty
+
     @cached_property
-    def value(self):
+    def _cached_value(self):
         return self._compute_function([
             input_.value for input_ in self._inputs
         ])
 
-
-class ReleasableCallback:
-
-    def __init__(self, notify, release):
-        self._notify = notify
-        self._release = release
-
-    def __call__(self, value=None):
-        self._notify()
-        return self._release
+    @property
+    def value(self):
+        if self.dirty:
+            del self._cached_value
+        return self._cached_value

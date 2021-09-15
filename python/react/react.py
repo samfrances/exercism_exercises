882830892ba352dc88f8422cd0989fee75bfd949
Sam Frances <sam@samfrances.uk>
2021-09-15 16:17:06 +0100

    Try to reduce inefficient recalculations

--- python/react/react.py
+++ python/react/react.py
@@ -1,15 +1,25 @@
 from functools import cached_property
 
-
 class Cell:
 
     @cached_property
     def _callbacks(self):
         return set([])
 
+    @property
+    def updating(self):
+        return self._updating
+
     def _notify_callbacks(self):
+        release_callbacks = []
+
         for callback in self._callbacks:
-            callback(self.value)
+            release = callback(self.value)
+            if callable(release):
+                release_callbacks.append(release)
+
+        for release in release_callbacks:
+            release()
 
     def add_callback(self, callback):
         self._callbacks.add(callback)
@@ -38,8 +48,16 @@ class ComputeCell(Cell):
         self._inputs = inputs
         self._compute_function = compute_function
         for input_ in self._inputs:
-            input_.add_callback(self._notify_on_change)
+            input_.add_callback(
+                ReleasableCallback(
+                    self._recalculate,
+                    self._notify_on_change
+                )
+            )
         self._previous_value = self.value
+
+    def _recalculate(self):
+        del self.value
 
     def _notify_on_change(self, value=None):
         new_value = self.value
@@ -47,8 +65,19 @@ class ComputeCell(Cell):
             self._previous_value = new_value
             self._notify_callbacks()
 
-    @property
+    @cached_property
     def value(self):
         return self._compute_function([
             input_.value for input_ in self._inputs
         ])
+
+
+class ReleasableCallback:
+
+    def __init__(self, notify, release):
+        self._notify = notify
+        self._release = release
+
+    def __call__(self, value=None):
+        self._notify()
+        return self._release

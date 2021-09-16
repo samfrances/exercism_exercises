07448efcd035150d601a5c95f81726eb51388364
Sam Frances <sam@samfrances.uk>
2021-09-16 08:49:28 +0100

    First iteration for bird-count exercise

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -1,21 +1,37 @@
 defmodule BirdCount do
-  def today(list) do
-    # Please implement the today/1 function
-  end
+  @type birdcounts :: list(non_neg_integer)
 
-  def increment_day_count(list) do
-    # Please implement the increment_day_count/1 function
-  end
+  @spec today(birdcounts) :: non_neg_integer
+  def today([]), do: nil
 
+  def today([this_day | _] = counts)
+      when length(counts) > 0,
+      do: this_day
+
+  @spec increment_day_count(birdcounts) :: birdcounts
+  def increment_day_count([]), do: [1]
+
+  def increment_day_count([today | rest] = counts)
+      when length(counts) >= 1,
+      do: [today + 1 | rest]
+
+  @spec has_day_without_birds?(birdcounts) :: boolean
   def has_day_without_birds?(list) do
-    # Please implement the has_day_without_birds?/1 function
+    list
+    |> Enum.any?(&(&1 == 0))
   end
 
+  @spec total(birdcounts) :: non_neg_integer
   def total(list) do
-    # Please implement the total/1 function
+    Enum.sum(list)
   end
 
+  @spec busy_days(birdcounts) :: non_neg_integer
   def busy_days(list) do
-    # Please implement the busy_days/1 function
+    list
+    |> Enum.filter(&busy?/1)
+    |> Enum.count()
   end
+
+  defp busy?(day), do: day >= 5
 end

2aaec3459389e82fea3fdf475faf3f33e51e7c80
Sam Frances <sam@samfrances.uk>
2021-09-18 09:34:26 +0100

    Variation of resistor color

--- elixir/resistor-color/lib/resistor_color.ex
+++ elixir/resistor-color/lib/resistor_color.ex
@@ -3,16 +3,22 @@ defmodule ResistorColor do
   Return the value of a color band
   """
 
-  @spec code(atom) :: integer()
-  def code(:black), do: 0
-  def code(:brown), do: 1
-  def code(:red), do: 2
-  def code(:orange), do: 3
-  def code(:yellow), do: 4
-  def code(:green), do: 5
-  def code(:blue), do: 6
-  def code(:violet), do: 7
-  def code(:grey), do: 8
-  def code(:white), do: 9
+  @resistor_values [
+    :black,
+    :brown,
+    :red,
+    :orange,
+    :yellow,
+    :green,
+    :blue,
+    :violet,
+    :grey,
+    :white
+  ]
+
+
+  def code(code) when code in @resistor_values do
+    Enum.find_index(@resistor_values, fn x -> x == code end)
+  end
 
 end

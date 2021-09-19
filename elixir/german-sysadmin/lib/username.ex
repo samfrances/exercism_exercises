4c41b3883a9ecd92a34de0489d48686f2c994d71
Sam Frances <sam@samfrances.uk>
2021-09-19 09:39:25 +0100

    Solution to german-sysadmin elixir exercise

--- elixir/german-sysadmin/lib/username.ex
+++ elixir/german-sysadmin/lib/username.ex
@@ -1,10 +1,27 @@
 defmodule Username do
-  def sanitize(username) do
-    # ä becomes ae
-    # ö becomes oe
-    # ü becomes ue
-    # ß becomes ss
 
-    # Please implement the sanitize/1 function
+  @spec sanitize(charlist) :: charlist
+  def sanitize(username) do
+    username
+    |> Enum.flat_map(&translate_german_char/1)
+    |> Enum.filter(&is_permitted_char/1)
+  end
+
+  defp translate_german_char(ch) do
+    case ch do
+      ?ä -> 'ae'
+      ?ö -> 'oe'
+      ?ü -> 'ue'
+      ?ß -> 'ss'
+      c -> [c]
+    end
+  end
+
+  defp is_permitted_char(ch) do
+    case ch do
+      ?_ -> true
+      ch when ch in ?a..?z -> true
+      _ -> false
+    end
   end
 end

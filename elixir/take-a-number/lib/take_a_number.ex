831082696592c164e5e4763723ddad827e823517
Sam Frances <sam@samfrances.uk>
2021-09-20 21:28:34 +0100

    First iteration of take-a-number

--- elixir/take-a-number/lib/take_a_number.ex
+++ elixir/take-a-number/lib/take_a_number.ex
@@ -1,5 +1,20 @@
 defmodule TakeANumber do
   def start() do
-    # Please implement the start/0 function
+    spawn(&run/0)
+  end
+
+  defp run(state \\ 0) do
+    receive do
+      {:report_state, sender_pid} ->
+        send(sender_pid, state)
+        run(state)
+      {:take_a_number, sender_pid} ->
+        new_state = state + 1
+        send(sender_pid, new_state)
+        run(new_state)
+      :stop -> nil
+      _ -> run(state)
+    end
+
   end
 end

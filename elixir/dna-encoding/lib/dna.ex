c0b0253eb823b88740abc9fa30f669f1f04f7e89
Sam Frances <sam@samfrances.uk>
2021-09-21 14:03:26 +0100

    Add typespec

--- elixir/dna-encoding/lib/dna.ex
+++ elixir/dna-encoding/lib/dna.ex
@@ -1,16 +1,25 @@
 defmodule DNA do
+
+  @type nucleotide :: ?A | ?C | ?G | ?T
+  @type space :: ?\s
+  @type encoded_nucleotide :: 1 | 2 | 4 | 8
+  @type encoded_space :: 0
+
+  @spec encode_nucleotide(nucleotide|space) :: encoded_nucleotide|encoded_space
   def encode_nucleotide(?\s), do: 0b0000
   def encode_nucleotide(?A), do: 0b0001
   def encode_nucleotide(?C), do: 0b0010
   def encode_nucleotide(?G), do: 0b0100
   def encode_nucleotide(?T), do: 0b1000
 
+  @spec decode_nucleotide(encoded_nucleotide|encoded_space) :: nucleotide|space
   def decode_nucleotide(0b0000), do: ?\s
   def decode_nucleotide(0b0001), do: ?A
   def decode_nucleotide(0b0010), do: ?C
   def decode_nucleotide(0b0100), do: ?G
   def decode_nucleotide(0b1000), do: ?T
 
+  @spec encode([nucleotide|space]) :: bitstring
   def encode(dna) do
     dna
     |> Enum.reverse()
@@ -23,6 +32,7 @@ defmodule DNA do
     encode_tail(rest, << encoded_first::4, result::bitstring >>)
   end
 
+  @spec decode(bitstring) :: [nucleotide|space]
   def decode(dna) do
     decode_tail(dna, [])
     |> Enum.reverse()

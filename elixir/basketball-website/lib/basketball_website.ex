4f5882b258567c0546d9d95a668ec04f24db44cd
Sam Frances <sam@samfrances.uk>
2021-09-22 17:52:53 +0100

    First iteration of basketball-website elixir task

--- elixir/basketball-website/lib/basketball_website.ex
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -1,9 +1,23 @@
 defmodule BasketballWebsite do
+  @spec extract_from_path(Map.t, String.t) :: any
   def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
+    path
+    |> String.split(".")
+    |> Enum.reduce_while(data, &traverse/2)
   end
 
+  defp traverse(key, data) do
+    next = data[key]
+    {
+      (if is_nil(next), do: :halt, else: :cont),
+      next
+    }
+  end
+
+  @spec get_in_path(Map.t, String.t) :: any
   def get_in_path(data, path) do
-    # Please implement the get_in_path/2 function
+    path
+    |> String.split(".")
+    |> then(& get_in data, &1)
   end
 end

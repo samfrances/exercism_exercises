2b473aa10fe0ada7db5a93b4f52f7d2d1d207836
Sam Frances <sam@samfrances.uk>
2021-09-22 19:57:32 +0100

    First iteration of collatz-conjecture

--- elixir/collatz-conjecture/lib/collatz_conjecture.ex
+++ elixir/collatz-conjecture/lib/collatz_conjecture.ex
@@ -5,7 +5,25 @@ defmodule CollatzConjecture do
     - if number is odd, multiply with 3 and add 1
     - if number is even, divide by 2
   """
+  require Integer
+
   @spec calc(input :: pos_integer()) :: non_neg_integer()
-  def calc(input) do
+  def calc(input) when is_integer(input) and input > 0 do
+    input
+    |> sequence()
+    |> Enum.count()
+  end
+
+  defp sequence(n) do
+    n
+    |> Stream.iterate(&collatz_step/1)
+    |> Stream.take_while(& &1 !== 1)
+  end
+
+  defp collatz_step(n) when Integer.is_even(n) do
+    div(n, 2)
+  end
+  defp collatz_step(n) do
+    (3 * n) + 1
   end
 end

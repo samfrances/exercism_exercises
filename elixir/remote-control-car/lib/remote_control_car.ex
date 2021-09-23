b4540bdd8f0df1f7c8b94078ba48320d8f7e7299
Sam Frances <sam@samfrances.uk>
2021-09-23 07:51:04 +0100

    Add typespecs to remote-control-car exercise

--- elixir/remote-control-car/lib/remote_control_car.ex
+++ elixir/remote-control-car/lib/remote_control_car.ex
@@ -1,5 +1,11 @@
 defmodule RemoteControlCar do
 
+  @type t :: %__MODULE__{
+    battery_percentage: non_neg_integer,
+    distance_driven_in_meters: non_neg_integer,
+    nickname: String.t
+  }
+
   @enforce_keys [:nickname]
   defstruct [
     battery_percentage: 100,
@@ -7,18 +13,22 @@ defmodule RemoteControlCar do
     nickname: nil
   ]
 
+  @spec new :: RemoteControlCar.t
   def new() do
     %__MODULE__{nickname: "none"}
   end
 
+  @spec new(String.t) :: RemoteControlCar.t
   def new(nickname) do
     %__MODULE__{nickname: nickname}
   end
 
+  @spec display_distance(RemoteControlCar.t) :: String.t
   def display_distance(%__MODULE__{distance_driven_in_meters: distance}) do
     "#{distance} meters"
   end
 
+  @spec display_battery(RemoteControlCar.t) :: String.t
   def display_battery(%__MODULE__{battery_percentage: battery}) do
     format_battery(battery)
   end
@@ -26,6 +36,7 @@ defmodule RemoteControlCar do
   defp format_battery(0), do: "Battery empty"
   defp format_battery(n), do: "Battery at #{n}%"
 
+  @spec drive(RemoteControlCar.t) :: RemoteControlCar.t
   def drive(car = %__MODULE__{battery_percentage: 0}) do
     car
   end

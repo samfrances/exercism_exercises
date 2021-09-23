45db95a2acf12a7105de7fe6c6e792cb159318ff
Sam Frances <sam@samfrances.uk>
2021-09-23 16:43:29 +0100

    Complete boutique-inventory elixir exercise

--- elixir/boutique-inventory/lib/boutique_inventory.ex
+++ elixir/boutique-inventory/lib/boutique_inventory.ex
@@ -1,17 +1,43 @@
 defmodule BoutiqueInventory do
+
+  @type item :: %{
+    name: String.t,
+    price: non_neg_integer|nil,
+    quantity_by_size: %{
+      s: non_neg_integer,
+      m: non_neg_integer,
+      l: non_neg_integer,
+      xl: non_neg_integer
+    }
+  }
+
+  @type inventory :: [item]
+
+  @spec sort_by_price(inventory) :: inventory
   def sort_by_price(inventory) do
-    # Please implement the sort_by_price/1 function
+    inventory
+    |> Enum.sort(fn x, y -> x.price <= y.price end)
   end
 
+  @spec with_missing_price(inventory) :: inventory
   def with_missing_price(inventory) do
-    # Please implement the with_missing_price/1 function
+    inventory
+    |> Enum.filter(fn x -> is_nil(x.price) end)
   end
 
+  @spec increase_quantity(item, non_neg_integer) :: item
   def increase_quantity(item, count) do
-    # Please implement the increase_quantity/2 function
+    quantities =
+      item.quantity_by_size
+      |> Enum.map(fn {size, n} -> {size, n + count} end)
+      |> Enum.into(%{})
+    %{ item | quantity_by_size: quantities }
   end
 
+  @spec total_quantity(item) :: non_neg_integer
   def total_quantity(item) do
-    # Please implement the total_quantity/1 function
+    item.quantity_by_size
+    |> Map.values()
+    |> Enum.sum()
   end
 end

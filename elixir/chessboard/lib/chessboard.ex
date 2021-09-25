417b379f90c277abe55e55c87d2b32302d9e1c6c
Sam Frances <sam@samfrances.uk>
2021-09-25 20:34:37 +0100

    chessboard first iteration

--- elixir/chessboard/lib/chessboard.ex
+++ elixir/chessboard/lib/chessboard.ex
@@ -1,17 +1,20 @@
 defmodule Chessboard do
   def rank_range do
-    # Please implement the rank_range/0 function
+    1..8
   end
 
   def file_range do
-    # Please implement the file_range/0 function
+    ?A..?H
   end
 
   def ranks do
-    # Please implement the ranks/0 function
+    Enum.to_list(rank_range())
   end
 
   def files do
-    # Please implement the files/0 function
+    file_range()
+    |> Enum.map(& [&1])
+    |> Enum.map(&List.to_string/1)
+    |> Enum.to_list()
   end
 end

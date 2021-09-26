df96f1877e8eb222af471bb4ca804aeacbad535b
Sam Frances <sam@samfrances.uk>
2021-09-26 10:57:25 +0100

    First iteration of boutique_suggestions elixir exercise

--- elixir/boutique-suggestions/lib/boutique_suggestions.ex
+++ elixir/boutique-suggestions/lib/boutique_suggestions.ex
@@ -1,5 +1,13 @@
 defmodule BoutiqueSuggestions do
-  def get_combinations(tops, bottoms, options) do
-    # Please implement the get_combinations/3 function
+  @default_options [maximum_price: 100]
+
+  def get_combinations(tops, bottoms, options \\ []) do
+    options = Keyword.merge(@default_options, options)
+    for top = %{base_color: top_base, price: top_price} <- tops,
+        bottom = %{base_color: bottom_base, price: bottom_price} <- bottoms,
+        top_base != bottom_base,
+        top_price + bottom_price <= options[:maximum_price] do
+      {top, bottom}
+    end
   end
 end

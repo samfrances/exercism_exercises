85313e5f257baa243509fb19f41a92be8af225ef
Sam Frances <sam@samfrances.uk>
2021-09-27 08:25:28 +0100

    bread-and-potions first iteration

--- elixir/bread-and-potions/lib/rpg.ex
+++ elixir/bread-and-potions/lib/rpg.ex
@@ -1,5 +1,7 @@
 defmodule RPG do
   defmodule Character do
+    @type t :: %__MODULE__{health: number(), mana: number()}
+
     defstruct health: 100, mana: 0
   end
 
@@ -19,5 +21,40 @@ defmodule RPG do
     defstruct []
   end
 
-  # Add code to define the protocol and its implementations below here...
+  defprotocol Edible do
+    @spec eat(t, RPG.Character.t) ::
+      {byproduct :: any, updated_character :: RPG.Character.t}
+    def eat(item, character)
+  end
+
+  defimpl Edible, for: LoafOfBread do
+    @spec eat(%RPG.LoafOfBread{}, RPG.Character.t) :: {nil, RPG.Character.t}
+    def eat(_item, character) do
+      {
+        nil,
+        %{character | health: character.health + 5}
+      }
+    end
+  end
+
+  defimpl Edible, for: ManaPotion do
+    @spec eat(%RPG.ManaPotion{}, RPG.Character.t()) :: {%RPG.EmptyBottle{}, RPG.Character.t()}
+    def eat(%RPG.ManaPotion{strength: strength}, character = %RPG.Character{mana: mana}) do
+      {
+        %RPG.EmptyBottle{},
+        %{character | mana: mana + strength}
+      }
+    end
+  end
+
+  defimpl Edible, for: Poison do
+    @spec eat(%RPG.Poison{}, RPG.Character.t()) :: {%RPG.EmptyBottle{}, RPG.Character.t()}
+    def eat(_poison, character) do
+      {
+        %RPG.EmptyBottle{},
+        %{character | health: 0}
+      }
+    end
+  end
+
 end

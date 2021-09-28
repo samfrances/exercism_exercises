b51eb82be01038a90936566b6c6cf8e9e669f421
Sam Frances <sam@samfrances.uk>
2021-09-28 21:46:49 +0100

    First iteration of nucleotide-count in Haskell

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,8 +1,24 @@
 module DNA (nucleotideCounts, Nucleotide(..)) where
 
-import Data.Map (Map)
+import Data.Map (Map, fromListWith)
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = error "You need to implement this function."
+nucleotideCounts xs = count <$> dnaFromString xs
+
+    where count :: [Nucleotide] -> Map Nucleotide Int
+          count = fromListWith (+) . map (\n -> (n, 1))
+
+          dnaFromString :: String -> Either String [Nucleotide]
+          dnaFromString = traverse nucleotideFromChar
+
+          nucleotideFromChar :: Char -> Either String Nucleotide
+          nucleotideFromChar 'A' = Right A
+          nucleotideFromChar 'C' = Right C
+          nucleotideFromChar 'G' = Right G
+          nucleotideFromChar 'T' = Right T
+          nucleotideFromChar x = Left [x]
+
+
+

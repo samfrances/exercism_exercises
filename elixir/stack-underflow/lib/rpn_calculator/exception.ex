aac288783b290b08177538137372f20691d2a97e
Sam Frances <sam@samfrances.uk>
2021-10-03 08:54:11 +0100

    First iteration of stack-underflow elixir exercise

--- elixir/stack-underflow/lib/rpn_calculator/exception.ex
+++ elixir/stack-underflow/lib/rpn_calculator/exception.ex
@@ -1,5 +1,30 @@
 defmodule RPNCalculator.Exception do
   # Please implement DivisionByZeroError here.
+  defmodule DivisionByZeroError do
+    defexception [message: "division by zero occurred"]
+  end
 
   # Please implement StackUnderflowError here.
+  defmodule StackUnderflowError do
+    @message "stack underflow occurred"
+
+    defexception [message: @message]
+
+    def exception(value) do
+      case value do
+        [] -> %StackUnderflowError{}
+        context -> %StackUnderflowError{message: "#{@message}, context: #{context}"}
+      end
+    end
+  end
+
+  def divide(stack) when length(stack) < 2 do
+    raise StackUnderflowError, "when dividing"
+  end
+  def divide([0|_]) do
+    raise DivisionByZeroError
+  end
+  def divide([a,b|_]) do
+    div(b, a)
+  end
 end

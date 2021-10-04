20ea248d869ea89de24c9ee8fdbd814a231e3d55
Sam Frances <sam@samfrances.uk>
2021-10-04 21:33:07 +0100

    First iteration of rpm-calculator-inspection

--- elixir/rpn-calculator-inspection/lib/rpn_calculator_inspection.ex
+++ elixir/rpn-calculator-inspection/lib/rpn_calculator_inspection.ex
@@ -1,17 +1,36 @@
 defmodule RPNCalculatorInspection do
   def start_reliability_check(calculator, input) do
-    # Please implement the start_reliability_check/2 function
+    {:ok, pid } = Task.start_link(fn -> calculator.(input) end)
+    %{input: input, pid: pid}
   end
 
   def await_reliability_check_result(%{pid: pid, input: input}, results) do
-    # Please implement the await_reliability_check_result/2 function
+    caller = self()
+    Task.start(fn -> Process.send_after(caller, :timeout, 100) end)
+    status = receive do
+      {:EXIT, _from = ^pid, _reason = :normal} -> :ok
+      {:EXIT, _from = ^pid, _reason} -> :error
+      :timeout -> :timeout
+    end
+    Map.put(results, input, status)
   end
 
   def reliability_check(calculator, inputs) do
-    # Please implement the reliability_check/2 function
+    previous_trap_exit = Keyword.get(Process.info(self()), :trap_exit)
+    Process.flag(:trap_exit, true)
+    try do
+      inputs
+        |> Enum.map(& start_reliability_check(calculator, &1))
+        |> Enum.reduce(%{}, &await_reliability_check_result/2)
+    after
+      Process.flag(:trap_exit, previous_trap_exit)
+    end
   end
 
   def correctness_check(calculator, inputs) do
-    # Please implement the correctness_check/2 function
+    inputs
+      |> Enum.map(& fn -> calculator.(&1) end)
+      |> Enum.map(&Task.async/1)
+      |> Enum.map(fn task -> Task.await(task, 100) end)
   end
 end

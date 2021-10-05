4b3222cd1d0f2a35e281d1cce957e4acd26557cb
Sam Frances <sam@samfrances.uk>
2021-10-05 08:33:42 +0100

    First iteration of Lucas numbers

--- elixir/lucas-numbers/lib/lucas_numbers.ex
+++ elixir/lucas-numbers/lib/lucas_numbers.ex
@@ -5,7 +5,24 @@ defmodule LucasNumbers do
 
   E.g.: 2, 1, 3, 4, 7, 11, 18, 29, ...
   """
+  def generate(count) when not is_integer(count) or count < 1 do
+    raise ArgumentError, "count must be specified as an integer >= 1"
+  end
   def generate(count) do
-    # Please implement the generate/1 function
+    lucas_stream()
+    |> Stream.take(count)
+    |> Enum.to_list()
+  end
+
+  def lucas_stream() do
+    Stream.unfold(
+      [], fn
+        [] -> {2, [2]}
+        [2] -> {1, [1, 2]}
+        [n_1, n_2] ->
+          next_num = n_1 + n_2
+          {next_num, [next_num, n_1]}
+      end
+    )
   end
 end

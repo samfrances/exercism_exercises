177d8819d18a4f517ad7db4d416673d61efa0554
Sam Frances <sam@samfrances.uk>
2021-10-07 20:46:49 +0100

    Third iteration of acronym in elixir

--- elixir/acronym/lib/acronym.ex
+++ elixir/acronym/lib/acronym.ex
@@ -5,5 +5,10 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
+    ~r/(\s|\-+|_+)/
+    |> Regex.split(string, trim: true)
+    |> Stream.map(&String.first/1)
+    |> Stream.map(&String.upcase/1)
+    |> Enum.join("")
   end
 end

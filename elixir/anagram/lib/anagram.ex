6b30d5da68b0c7dd4fa37218460dbafd43a689e4
Sam Frances <sam@samfrances.uk>
2021-10-10 15:48:00 +0100

    First iteration of anagrams in elixir

--- elixir/anagram/lib/anagram.ex
+++ elixir/anagram/lib/anagram.ex
@@ -4,5 +4,20 @@ defmodule Anagram do
   """
   @spec match(String.t(), [String.t()]) :: [String.t()]
   def match(base, candidates) do
+    base_fingerprint = fingerprint(base)
+    candidates
+    |> Stream.reject(& String.downcase(&1) == String.downcase(base))
+    |> Stream.map(fn candidate -> {candidate, fingerprint(candidate)} end)
+    |> Stream.filter(fn {_candidate, fingerprint} -> fingerprint == base_fingerprint end)
+    |> Stream.map(& elem(&1, 0))
+    |> Enum.to_list()
   end
+
+  defp fingerprint(str) do
+    str
+    |> String.downcase()
+    |> String.graphemes()
+    |> Enum.sort()
+  end
+
 end

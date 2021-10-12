15b27c436bb689c60aed9ca73dcc3542b1be8b70
Sam Frances <sam@samfrances.uk>
2021-10-12 21:36:11 +0100

    Much simpler version of sumOfMultiples

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,16 +1,9 @@
 module SumOfMultiples (sumOfMultiples) where
 
-import qualified Data.Set as Set
-import Data.Set (unions)
+import Data.List
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
 sumOfMultiples factors limit =
-    sum $ combinedMultiples factors limit
-
-combinedMultiples :: [Integer] -> Integer -> Set.Set Integer
-combinedMultiples factors limit =
-    unions $ map Set.fromList $ map (multiples limit) factors
-
-multiples :: Integer -> Integer -> [Integer]
-multiples _limit 0 = [0]
-multiples limit n = [n,n+n..limit-1]
+    sum $ nub $ multiples =<< factors
+    where multiples 0 = []
+          multiples n = [n,n+n..limit-1]

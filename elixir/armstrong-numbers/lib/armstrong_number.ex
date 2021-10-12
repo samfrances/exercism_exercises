4a61ad9a13c40cc847ddadac28c53c9a87dab613
Sam Frances <sam@samfrances.uk>
2021-10-12 08:08:35 +0100

    armstrong-numbers first iteration

--- elixir/armstrong-numbers/lib/armstrong_number.ex
+++ elixir/armstrong-numbers/lib/armstrong_number.ex
@@ -5,5 +5,21 @@ defmodule ArmstrongNumber do
 
   @spec valid?(integer) :: boolean
   def valid?(number) do
+    the_digits = digits(number, [])
+    digit_count = length(the_digits)
+
+    the_digits
+    |> Enum.map(& Integer.pow(&1, digit_count))
+    |> Enum.sum()
+    |> Kernel.===(number)
+  end
+
+  defp digits(0, []), do: [0]
+  defp digits(0, result), do: result
+  defp digits(number, result) do
+    base = 10
+    quotient = div(number, base)
+    remainder = rem(number, base)
+    digits(quotient, [remainder | result])
   end
 end

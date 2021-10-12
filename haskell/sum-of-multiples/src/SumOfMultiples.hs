01d47773383e29c1351a1a876794cc59c81aa235
Sam Frances <sam@samfrances.uk>
2021-10-12 21:22:28 +0100

    First iteration of haskell SumOfMultiples

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,4 +1,16 @@
 module SumOfMultiples (sumOfMultiples) where
 
+import qualified Data.Set as Set
+import Data.Set (unions)
+
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = error "You need to implement this function."
+sumOfMultiples factors limit =
+    sum $ combinedMultiples factors limit
+
+combinedMultiples :: [Integer] -> Integer -> Set.Set Integer
+combinedMultiples factors limit =
+    unions $ map Set.fromList $ map (multiples limit) factors
+
+multiples :: Integer -> Integer -> [Integer]
+multiples _limit 0 = [0]
+multiples limit n = [n,n+n..limit-1]

1fdb053aa5573be42d895385a9b0e5dd6c2cdc95
Sam Frances <sam@samfrances.uk>
2025-05-05 09:05:17 +0100

    Haskell: secret handshake: second iteration

--- haskell/secret-handshake/src/SecretHandshake.hs
+++ haskell/secret-handshake/src/SecretHandshake.hs
@@ -1,27 +1,22 @@
 module SecretHandshake (handshake) where
 
 import Data.Bits
-import Data.List
+import Data.Maybe
 
 handshake :: Int -> [String]
-handshake n = orderActions n $ unfoldr (getNextAction n) 0
+handshake n = orderActions n $ mapMaybe (getAction n) [0..3]
 
-
-getNextAction :: Int -> Int -> Maybe (String, Int)
-
-getNextAction _ place
-    | place > 3 = Nothing
-    | place < 0 = Nothing
-
-getNextAction n actionBit = if actionIsSet then Just (action, nextBit) else getNextAction n nextBit
-                        where action = actions !! actionBit
-                              nextBit = actionBit + 1
-                              actionIsSet = testBit n actionBit
-                              actions = ["wink", "double blink", "close your eyes", "jump"]
-
+getAction :: Int -> Int -> Maybe String
+getAction n actionBit = if include then Just action else Nothing
+                            where action = actions !! actionBit
+                                  actions = ["wink", "double blink", "close your eyes", "jump"]
+                                  include = testBit n actionBit
 
 orderActions :: Int -> [String] -> [String]
 orderActions n actions = if (shouldReverse n) then reverse actions else actions
 
 shouldReverse :: Int -> Bool
 shouldReverse n = testBit n 4
+
+
+

d15c02fdfd903fb9b721eba37cddedcb6a5364cd
Sam Frances <sam@samfrances.uk>
2025-05-05 10:34:48 +0100

    Haskell: secret handshake: more concise version of foldr version

--- haskell/secret-handshake/src/SecretHandshake.hs
+++ haskell/secret-handshake/src/SecretHandshake.hs
@@ -8,19 +8,12 @@ handshake n = foldr (.) id (getActions n) $ []
 type ActionFn = [String] -> [String]
 
 getActions :: Int -> [ActionFn]
-getActions n = [(orderActions n), (jump n), (closeEyes n), (doubleBlink n),  (wink n)]
+getActions n = (orderActions n):commandActionFns
+                where commandActionFns = map (prependCommandAction n) $ reverse [0..3]
 
-wink :: Int -> ActionFn
-wink n actions = if testBit n 0 then "wink":actions else actions
-
-doubleBlink :: Int -> ActionFn
-doubleBlink n actions = if testBit n 1 then "double blink":actions else actions
-
-closeEyes :: Int -> ActionFn
-closeEyes n actions = if testBit n 2 then "close your eyes":actions else actions
-
-jump :: Int -> ActionFn
-jump n actions = if testBit n 3 then "jump":actions else actions
+prependCommandAction :: Int -> Int -> ActionFn
+prependCommandAction n actionBit actions = if testBit n actionBit then action:actions else actions
+                                where action = ["wink", "double blink", "close your eyes", "jump"] !! actionBit
 
 orderActions :: Int -> ActionFn
 orderActions n actions = if (testBit n 4) then actions else reverse actions
